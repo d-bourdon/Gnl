e73f3b21daedbbf38d0924dc6d605e274bdbf77e
Dylan BOURDON <dbourdon@e1r2p2.42.fr>
2016-02-08 16:06:27 +0100

    Save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/06 13:20:56 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/02/08 15:27:37 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/02/08 16:06:23 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,19 @@
 char	*readnext(int fd)
 {
 	char	*str;
-	str = (char*)malloc(sizeof(char) * BUFF_SIZE + 1)
+	str = (char*)malloc(sizeof(char) * BUFF_SIZE + 1);
+	if (read(fd, str, BUFF_SIZE))
+		return (str);
+	return(0);
 }
 
 int		get_next_line(int fd, **line)
 {
+	int			i;
+	static char	**tmp;
 
+	i = 0;
+	while (*tmp[i] != '\n' && *tmp[i] != '\0')
+		i++;
+	if (i == 0)
 }
