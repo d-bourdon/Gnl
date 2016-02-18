6def65218ba887a451fc7c574a4c26afc5e7784f
Dylan BOURDON <dbourdon@e1r2p4.42.fr>
2016-02-18 18:06:11 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -5,41 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/06 13:20:56 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/02/12 16:36:35 by dbourdon         ###   ########.fr       */
+/*   Created: 2016/02/18 14:06:31 by dbourdon          #+#    #+#             */
+/*   Updated: 2016/02/18 18:05:39 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-int		get_next_line(int fd,char **line)
+int		buff_stock_void (char *buff_stock, char *out)
 {
-	char	*str;
-	char	*final;
 	int		i;
-	int		ret;
 
 	i = 0;
-	ret = 0;
-	str = ft_strnew(1);
-	final = ft_strnew(1);
-	if (fd == -1 || line == NULL)
-		return (-1);
-	while (i <= BUFF_SIZE)
+	while (i >= 0)
 	{
-		ret = read(fd, str, 1);
-		if (ret == -1)
-			return(-1);
-		if (str[0] == '\n' || ret == 0)
-		{	
-			if (ret == 0)
-				return (0);
-			return (1);
+		if (buff_stock[i] == '\0')
+		{
+			if (i == 0);
+				return(0);
+			return(i);
 		}
-		final = ft_strjoin(final, str);
+		else if (buff_stock[i] == '\n')
+			ft_return(1);
+		else
+			out[i] == buff_stock[i];
 		i++;
-		*line = final;
 	}
-	return (1);
+}
+
+int		ft_return(int val)
+{
+	ghdjsad
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char	buff_stock;
+	int			nb;
+	char		out;
+
+	nb = buff_stock_void(buff_stock, out);
+	if (nb == 0)
+		//read total
+	else
+		//read de buffsize-nb
 }
