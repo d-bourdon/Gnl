ddf9e1fce6fd3721813e79639521b1cf0466e1c5
Dylan BOURDON <dbourdon@e1r13p1.42.fr>
2016-02-22 15:29:00 +0100

    Save

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,32 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 14:06:31 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/02/22 14:08:58 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/02/22 15:28:53 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		ft_read(buff_stock, fd)
+int		ft_read(char **buff_stock, int fd)
 {
 	int		ret;
 
 	ret = 0;
-	buff_stock = strnew(BUFF_SIZE);
-	ret = read(fd, buff_stock, BUFF_SIZE);
+	*buff_stock = ft_strnew(BUFF_SIZE);
+	if (*buff_stock == NULL)
+		return(-1);
+	ret = read(fd, *buff_stock, BUFF_SIZE);
 	return(ret);
 }
 
-int		ft_return(int ret, char *out, char **line)
+int		ft_return(int ret, char *out, char **line, char **buff_stock, int i)
 {
-	
+	*line = out;
+	*buff_stock = *buff_stock + (i + 1);
+	if (ret == 1)
+		return(1);
+	else
+		return(0);
 }
 
 int		get_next_line(int fd, char **line)
@@ -33,31 +40,33 @@ int		get_next_line(int fd, char **line)
 	char		*out;
 	int			i;
 	int			j;
-
+	int			nb;
 
 	i = 0;
 	j = 0;
+	nb = 0;
 	out = ft_strnew(BUFF_SIZE);
 	if (buff_stock == NULL)
-		if (ft_read(buff_stock, fd) == -1)
+		if (ft_read(&buff_stock, fd) == -1)
 			return(-1);
+	printf("Phrase: %s\n", buff_stock);
 	while (i < BUFF_SIZE - nb)
 	{
 		if (buff_stock[i] == '\n')
-			return(ft_return(1, out, line));
+			return(ft_return(1, out, line, &buff_stock, i));
 		else if (buff_stock[i] == '\0')
 		{
-			free(buff_stock);
-			if(ft_read(buff_stock, fd) == -1)
+			free(&buff_stock);
+			if(ft_read(&buff_stock, fd) == -1)
 				return(-1);
 			nb = i;
-			i = 0;
+			i = -1;
 		}
 		else if (buff_stock[i] == EOF)
-			return(ft_return(0, out, line));
+			return(ft_return(0, out, line, &buff_stock, i));
 		else
 			out[j++] = buff_stock[i];
 		i++;
 	}
-	return(ft_return(1, out, line));
+	return(ft_return(1, out, line, &buff_stock, i));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/06 13:33:17 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/02/11 17:48:04 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/02/22 15:03:00 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 2000
+# define BUFF_SIZE 20
 
 # include <stdio.h>
 # include <sys/types.h>
