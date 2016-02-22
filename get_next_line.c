6e4cf6b38b088e41e6b47373a6f39322c9cb2744
Dylan BOURDON <dbourdon@e1r13p1.42.fr>
2016-02-22 14:09:08 +0100

    Save

--- get_next_line.c
+++ get_next_line.c
@@ -6,49 +6,58 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 14:06:31 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/02/21 14:55:33 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/02/22 14:08:58 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		buff_stock_void (char *buff_stock, char *out, int limit)
+int		ft_read(buff_stock, fd)
 {
-	int		i;
+	int		ret;
 
-	i = 0;
-	while (i < limit)
-	{
-		if (buff_stock[i] == '\0')
-		{
-			if (i == 0);
-				return(0);
-			return(i);
-		}
-		else if (buff_stock[i] == '\n')
-			ft_return(1);
-		else
-			out[i] == buff_stock[i];
-		i++;
-	}
+	ret = 0;
+	buff_stock = strnew(BUFF_SIZE);
+	ret = read(fd, buff_stock, BUFF_SIZE);
+	return(ret);
 }
 
-int		ft_return(int val)
+int		ft_return(int ret, char *out, char **line)
 {
-	ghdjsad                 
+	
 }
 
 int		get_next_line(int fd, char **line)
 {
-	static char	buff_stock;
-	int			nb;
-	char		out;
+	static char *buff_stock;
+	char		*out;
+	int			i;
+	int			j;
+
+
+	i = 0;
+	j = 0;
+	out = ft_strnew(BUFF_SIZE);
 	if (buff_stock == NULL)
-		buff_stock = ft_strnew(BUFF_SIZE);
-	nb = buff_stock_void(buff_stock, out, BUFF_SIZE);
-	if (nb != -1)
+		if (ft_read(buff_stock, fd) == -1)
+			return(-1);
+	while (i < BUFF_SIZE - nb)
 	{
-		read(fd, buff_stock, BUFF_SIZE);
-		buff_stock_void(buff_stock, out, BUFF_SIZE - nb);
+		if (buff_stock[i] == '\n')
+			return(ft_return(1, out, line));
+		else if (buff_stock[i] == '\0')
+		{
+			free(buff_stock);
+			if(ft_read(buff_stock, fd) == -1)
+				return(-1);
+			nb = i;
+			i = 0;
+		}
+		else if (buff_stock[i] == EOF)
+			return(ft_return(0, out, line));
+		else
+			out[j++] = buff_stock[i];
+		i++;
 	}
+	return(ft_return(1, out, line));
 }
