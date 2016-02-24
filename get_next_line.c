ede484fa58cedbad5437c39a6d79622990400c3d
Dylan BOURDON <dbourdon@e1r13p1.42.fr>
2016-02-24 14:17:23 +0100

    Save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 14:06:31 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/02/24 11:57:03 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/02/24 14:10:22 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,22 @@ int		ft_read(char **buff_stock, int fd)
 	if (*buff_stock == NULL)
 		return(-1);
 	ret = read(fd, tmp, BUFF_SIZE);
-	printf("Je viens de lire: %s\n Mon Buff_stock est: %s\n", tmp, *buff_stock);
 	if (ret == -1)
 		return(-1);
 	*buff_stock = ft_strjoin(*buff_stock, tmp);
-	printf("Apres mon join: %s\n", *buff_stock);
 	free(tmp);
 	return(ret);
 }
 
-int		ft_return(int ret, char *out, char **line, char **buff_stock, int i)
+int		ft_return(char *out, char **line, char **buff_stock, int i)
 {
+	if(line == NULL)
+		return(-1);
+        :s
+        :s
 	*line = out;
 	*buff_stock = *buff_stock + (i + 1);
-	if (ret == 1)
+	if (out[0] != '\0' && out != NULL)
 		return(1);
 	else
 		return(0);
@@ -60,15 +62,15 @@ int		get_next_line(int fd, char **line)
 	if (buff_stock == NULL)
 		if (ft_read(&buff_stock, fd) == -1)
 			return(-1);
-	while (i < BUFF_SIZE - nb)
+	while (i < BUFF_SIZE)
 	{
 		if (buff_stock[i] == '\n')
-			return(ft_return(1, out, line, &buff_stock, i));
+			return(ft_return(out, line, &buff_stock, i));
 		else if (buff_stock[i] == '\0')
 		{
 			nb = ft_read(&buff_stock, fd);
 			if (nb == 0)
-				return(ft_return(0, out, line, &buff_stock, i));
+				return(ft_return(out, line, &buff_stock, i));
 			if (nb == -1)
 				return(-1);
 			nb = i;
@@ -76,10 +78,10 @@ int		get_next_line(int fd, char **line)
 			j = 0;
 		}
 		else if (buff_stock[i] == EOF)
-			return(ft_return(0, out, line, &buff_stock, i));
+			return(ft_return(out, line, &buff_stock, i));
 		else
 			out[j++] = buff_stock[i];
 		i++;
 	}
-	return(ft_return(1, out, line, &buff_stock, i));
+	return(ft_return(out, line, &buff_stock, i));
 }
